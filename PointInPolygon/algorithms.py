769fb08ace2b98a5824278503a4105bb8f9a958b
kulichadam <122116569+kulichadam@users.noreply.github.com>
2023-03-08 09:48:53 +0100

    wn

--- PointInPolygon/algorithms.py
+++ PointInPolygon/algorithms.py
@@ -42,38 +42,33 @@ class Algorithms:
 
         n = len(pol)
         uhel = 0
+        # print(pol[0].x())
 
-        for i in range(n-1):
-            #print(i)
+        for i in range(n - 1):
+
             xir = pol[i].x() - q.x()
             yir = pol[i].y() - q.y()
-            xi1r = pol[(i+1)%n].x() - q.x()
-            yi1r = pol[(i+1)%n].y() - q.y()
-            #print(xir, yir, xi1r, yi1r)
-            xm = (xir * yi1r - xi1r * yir) / (yi1r - yir)
-            #print('Ano')
-            #print('-----', pol[i].y(), pol[(i+1)%n].y())
-            #print('-----', pol[i].x(), pol[(i + 1) % n].x())
-            #print('-----', yir, yi1r)
-            scalar = xir*xi1r + yir*yi1r
+            xi1r = pol[(i + 1) % n].x() - q.x()
+            yi1r = pol[(i + 1) % n].y() - q.y()
 
-            length = (xir**2 + yir**2)**(1/2) * (xi1r**2 + yi1r**2)**(1/2)
-            #print(i, '-', scalar / length, 'scalar / length')
+            xm = (xi1r * yir - xir * yi1r) / (yi1r - yir)
+
+            scalar = xir * xi1r + yir * yi1r
+
+            length = (xir ** 2 + yir ** 2) ** (1 / 2) * (xi1r ** 2 + yi1r ** 2) ** (1 / 2)
+
             fi = np.arccos(scalar / length)
-            #print(i, '-', fi, 'fi')
-            #print(i)
 
-            if pol[i].y() > pol[(i+1)%n].y():  # orientace hrany je nahoru
+            if pol[i].y() > pol[(i + 1) % n].y():  # orientace hrany je nahoru
                 if xm > 0:  # bod je nalevo od hrany ve smeru orientace (+)
                     uhel += fi
                 elif xm < 0:  # bod je napravo od hrany (-)
                     uhel -= fi
-            elif pol[i].y() < pol[(i+1)%n].y():  # orientace hrany je dolu
-                if xm < 0:  # bod je nalevo od hrany (-)
+            elif pol[i].y() < pol[(i + 1) % n].y():  # orientace hrany je dolu
+                if xm < 0:  # bod je nalevo od hrany (+)
                     uhel += fi
-                elif xm > 0:  # bod je napravo od hrany (+)
+                elif xm > 0:  # bod je napravo od hrany (-)
                     uhel -= fi
-            #print(i, '-', uhel, 'uhel')
 
         if uhel > 6.2:
             return 1
